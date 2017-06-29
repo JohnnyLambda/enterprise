68adc2feebf43fc8f92d07f77ae88ab25b4086ca
William Edwin Sharkey <williamsharkey@gmail.com>
2017-06-29 19:29:33 -0400

    store result of test to prevent compiler from removing call

--- enterprise_test.go
+++ enterprise_test.go
@@ -18,3 +18,29 @@ func BenchmarkHello(b *testing.B) {
 		fmt.Sprintf("hello")
 	}
 }
+
+var result string
+
+// TestHelloComplete tests if hello can be printed.
+func TestHelloComplete(t *testing.T) {
+
+	// always record the result of printing to prevent
+	// the compiler eliminating the function call.
+
+	var s string
+	s = fmt.Sprintf("hello")
+	result = s
+}
+
+// BenchmarkHelloComplete tests how fast hello can be printed.
+func BenchmarkHelloComplete(b *testing.B) {
+
+	// always record the result of printing to prevent
+	// the compiler eliminating the function call.
+
+	var s string
+	for i := 0; i < b.N; i++ {
+		s = fmt.Sprintf("hello")
+	}
+	result = s
+}
