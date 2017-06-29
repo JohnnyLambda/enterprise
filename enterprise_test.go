85957176f65a4b6b1ca253420a5c6e80d6dfb9f2
William Edwin Sharkey <williamsharkey@gmail.com>
2017-06-29 19:16:33 -0400

    Add TestHello, and rename test suite.

--- enterprise_test.go
+++ enterprise_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+// TestHello tests if hello can be printed.
+func TestHello(t *testing.T) {
+
+	fmt.Sprintf("hello")
+
+}
+
 // BenchmarkHello tests how fast hello can be printed.
 func BenchmarkHello(b *testing.B) {
 	for i := 0; i < b.N; i++ {
