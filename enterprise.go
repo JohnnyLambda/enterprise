f9097b5f34d229222ded6c081841571ab3c2e5f2
William Edwin Sharkey <williamsharkey@gmail.com>
2017-07-27 20:20:48 -0400

    Add add() function and tests

---
+++ enterprise.go
@@ -0,0 +1,6 @@
+package enterprise
+
+func add(a int, b int) (c int) {
+	c = a + b
+	return c
+}
--- enterprise_test.go
+++ enterprise_test.go
@@ -44,3 +44,25 @@ func BenchmarkHelloComplete(b *testing.B) {
 	}
 	result = s
 }
+
+func Test_add(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantC int
+	}{
+		{name: "easy", args: args{a: 1, b: 2}, wantC: 3},
+		{name: "neg", args: args{a: -1, b: -2}, wantC: -3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotC := add(tt.args.a, tt.args.b); gotC != tt.wantC {
+				t.Errorf("add() = %v, want %v", gotC, tt.wantC)
+			}
+		})
+	}
+}
